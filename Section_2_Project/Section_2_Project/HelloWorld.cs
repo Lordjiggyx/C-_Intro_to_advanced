89520f39c315c475479eb99280368caa92969198
tl-Tomi-Ilori <tomiilori123@gmail.com>
2022-11-01 12:42:14 +0000

    Subsection 17
    creating the first project in rider

---
+++ Section_2_Project/Section_2_Project/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Section_2_Project
+{
+    public class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
